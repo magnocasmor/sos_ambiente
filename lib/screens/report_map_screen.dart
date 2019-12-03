0e9e35bab7499b02458decae7a8519dde332c646
Magno Castro Moraes <magnocastro@Magnos-Mac-mini.local>
2019-12-03 14:45:48 -0300

    Add marker

--- lib/screens/report_map_screen.dart
+++ lib/screens/report_map_screen.dart
@@ -3,12 +3,19 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class ReportMapScreen extends StatelessWidget {
+class ReportMapScreen extends StatefulWidget {
   ReportMapScreen({Key key}) : super(key: key);
 
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+
+  @override
+  _ReportMapScreenState createState() => _ReportMapScreenState();
+}
+
+class _ReportMapScreenState extends State<ReportMapScreen> {
   Completer<GoogleMapController> _controller = Completer();
 
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
+  final Set<Marker> _markers = Set<Marker>();
 
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
@@ -19,9 +26,25 @@ class ReportMapScreen extends StatelessWidget {
     return GoogleMap(
       onMapCreated: _onMapCreated,
       initialCameraPosition: CameraPosition(
-        target: _center,
+        target: ReportMapScreen._center,
         zoom: 11.0,
       ),
+      markers: _markers,
+      trafficEnabled: false,
+      onLongPress: (latLong) {
+        setState(() {
+          _markers.add(
+            Marker(
+              markerId: MarkerId(
+                latLong.toString(),
+              ),
+              draggable: false,
+              position: latLong,
+              visible: true,
+            ),
+          );
+        });
+      },
     );
   }
 }
